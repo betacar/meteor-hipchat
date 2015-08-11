733ab423e12c1a20fd0c834bcd1ee8240e38ae72
Carlos Betancourt Carrero <betacar@x-wing.iptv\.microsoft\.com>
2015-08-11 12:12:34 -0300

    Deleted old package dependencies and settings

--- package.js
+++ package.js
@@ -1,14 +1,21 @@
 Package.describe({
   name: 'betacar:hipchat-notify',
-  version: '1.0.3',
-  summary: 'Wrapper for hipchat-notify.js@1.0.3',
+  version: '1.1.0',
+  summary: 'HipChat notifications for Meteor apps',
   git: 'git://github.com/betacar/meteor-hipchat.git',
   documentation: 'README.md',
   license: 'LICENSE'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@0.9.0');
-  api.addFiles('hipchat-notify.js/index.js', 'server');
+  api.versionsFrom('1.1.0.3');
+  api.use(['http'], 'server');
+
+  api.addFiles(['hipchat-notify.js'], 'server');
   api.export('HipChatNotify', 'server');
+});
+
+Package.onTest(function(api) {
+  api.use(['tinytest','betacar:hipchat-notify','http'], 'server');
+  api.addFiles('hipchat-notify-tests.js', 'server');
 });
